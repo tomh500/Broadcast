dca67e43d376bfb18ab25712797dfbc006a66170
tomh500 <95513888+tomh500@users.noreply.github.com>
2025-10-15 21:06:50 +0800

    Update Broadcast.java

--- src/main/com/sunqi/broadcast/Broadcast.java
+++ src/main/com/sunqi/broadcast/Broadcast.java
@@ -14,8 +14,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Broadcast extends JavaPlugin implements CommandExecutor, Listener {
 
     private boolean enabled = true;
-    private String itemDisplayName = ChatColor.BOLD + " " + ChatColor.AQUA + " %s";
-    private String playerDisplayName = ChatColor.BOLD + " " + ChatColor.YELLOW + " %s";
+    private String itemDisplayName;
+    private String playerDisplayName;
+    private String prefix;
+
     @Override
     public void onEnable() {
         getLogger().info("Broadcast plugin enabled!");
@@ -34,63 +36,59 @@ public class Broadcast extends JavaPlugin implements CommandExecutor, Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("broadcast")) {
             if (args.length == 0) {
-                sender.sendMessage(ChatColor.RED + "Usage: /broadcast <on|off> [itemDisplayName] [playerDisplayName]");
+                sender.sendMessage(ChatColor.RED + "Usage: /broadcast <on|off>");
                 return true;
             }
-            
+
             if (args[0].equalsIgnoreCase("on")) {
                 enabled = true;
+                getConfig().set("enabled", true);
                 saveConfig();
-                sender.sendMessage(ChatColor.GREEN + "Broadcast is now enabled.");
+                sender.sendMessage(ChatColor.GREEN + "击杀广播已开启！");
             } else if (args[0].equalsIgnoreCase("off")) {
                 enabled = false;
+                getConfig().set("enabled", false);
                 saveConfig();
-                sender.sendMessage(ChatColor.GREEN + "Broadcast is now disabled.");
+                sender.sendMessage(ChatColor.RED + "击杀广播已关闭！");
             } else {
-                sender.sendMessage(ChatColor.RED + "Invalid argument. Usage: /broadcast <on|off> [itemDisplayName] [playerDisplayName]");
+                sender.sendMessage(ChatColor.RED + "用法: /broadcast <on|off>");
             }
-            
-            if (args.length >= 2) {
-                itemDisplayName = ChatColor.translateAlternateColorCodes('&', args[1]);
-            }
-            
-            if (args.length >= 3) {
-                playerDisplayName = ChatColor.translateAlternateColorCodes('&', args[2]);
-            }
-            
             return true;
         }
-        
         return false;
     }
-    
+
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
         if (!enabled || !(event.getEntity().getKiller() instanceof Player)) {
             return;
         }
-        
+
         Player killer = event.getEntity().getKiller();
         ItemStack weapon = killer.getInventory().getItemInMainHand();
-        
+        String itemName;
+
         if (weapon != null && weapon.hasItemMeta() && weapon.getItemMeta().hasDisplayName()) {
-            String itemName = weapon.getItemMeta().getDisplayName();
-            String killerName = killer.getName();
-            String entityName = event.getEntity().getName();
-            
-            String message = String.format("%s使用%s击杀了%s", 
-                    String.format(playerDisplayName, killerName), String.format(itemDisplayName, itemName), ChatColor.GREEN + entityName);
-            
-            getServer().broadcastMessage(message);
+            itemName = weapon.getItemMeta().getDisplayName() + ChatColor.RESET;
+        } else {
+            itemName = ChatColor.GRAY + "空手" + ChatColor.RESET;
         }
+
+        String killerName = String.format(playerDisplayName, killer.getName()) + ChatColor.RESET;
+        String entityName = ChatColor.RED + event.getEntity().getName() + ChatColor.RESET;
+
+        String message = prefix + " " + killerName + ChatColor.WHITE + " 使用 " + itemName + ChatColor.WHITE + " 击杀了 " + entityName;
+        getServer().broadcastMessage(message);
     }
-    
+
     private void loadConfig() {
         reloadConfig();
         enabled = getConfig().getBoolean("enabled", true);
-        itemDisplayName = getConfig().getString("itemDisplayName", itemDisplayName);
-        playerDisplayName = getConfig().getString("playerDisplayName", playerDisplayName);
-        itemDisplayName = ChatColor.translateAlternateColorCodes('&', itemDisplayName);
-        playerDisplayName = ChatColor.translateAlternateColorCodes('&', playerDisplayName);
+        itemDisplayName = ChatColor.translateAlternateColorCodes('&',
+                getConfig().getString("itemDisplayName", "&l&b%s&r"));
+        playerDisplayName = ChatColor.translateAlternateColorCodes('&',
+                getConfig().getString("playerDisplayName", "&l&e%s&r"));
+        prefix = ChatColor.translateAlternateColorCodes('&',
+                getConfig().getString("prefix", "&6[&e击杀广播&6]&r"));
     }
 }
